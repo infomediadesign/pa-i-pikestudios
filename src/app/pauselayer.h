842ca31bdab26ebbec1a823154a61fe251417f71
no8f <snipez5002@gmail.com>
2025-12-02 11:43:25 +0100

    fixed cmake list

---
+++ src/app/pauselayer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <psinterfaces/layer.h>
+#include <raylib.h>
+
+class PauseLayer: public PSInterfaces::Layer
+{
+	public:
+	PauseLayer();
+	
+	void on_update(const float dt) override;
+	void on_render(const float dt) override;
+	
+	enum BtnState {
+		Idle = 0,
+		Hovered,
+		Down
+	};
+	
+	private:
+	Rectangle m_quit_bounding_rect;
+	BtnState m_current_quit_state = Idle;
+};
