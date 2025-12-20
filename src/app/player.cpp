3d0c8d01cef73aeeff9c18a1fb7740fe33740802
Spielemeistr <mppb1908@gmx.de>
2025-12-20 17:32:06 +0100

    Merging Movement Controller

--- src/app/player.cpp
+++ src/app/player.cpp
@@ -8,12 +8,24 @@
 Player::Player() {
 	
 	// WARNING: THIS IS ONLY FOR TESTING
-	m_pos = {GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
-	m_rot = 0;
-	set_texture_value();
+	m_position = {GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
+	m_max_velocity = 600;
+	m_rotation = 0;
+	set_interpolation_values(6, 2, 4, 1500, 0.3);
+	set_texture_value(LoadTexture("ressources/SpaceShip.png"), 5, 90, 0.5);
 	//
 	
 	PSCore::Application::get()->register_entity(this);
+}
+
+Vector2 Player::position()
+{
+	return m_position;
+}
+
+Vector2 Player::velocity()
+{
+	return m_velocity;
 }
 
 float Player::target_velocity()
@@ -21,9 +33,30 @@ float Player::target_velocity()
 	return m_target_velocity;
 }
 
+float Player::max_velocity()
+{
+	return m_max_velocity;
+}
+
+float Player::rotation()
+{
+	return m_rotation;
+}
+
 float Player::target_rotation()
 {
 	return m_target_rotation;
+}
+
+void Player::set_position(const Vector2& position)
+{
+	m_position = position;
+}
+
+void Player::set_velocity(const Vector2& velocity)
+{
+	m_velocity = Vector2Length(velocity) <= m_max_velocity ? velocity : Vector2Scale(Vector2Normalize(velocity), m_max_velocity);
+	m_target_velocity = Vector2Length(velocity) <= m_max_velocity ? Vector2Length(velocity) : m_max_velocity;
 }
 
 void Player::set_target_velocity(const float& target_velocity)
@@ -31,47 +64,70 @@ void Player::set_target_velocity(const float& target_velocity)
 	m_target_velocity = target_velocity;
 }
 
+void Player::set_max_velocity(const float& max_velocity)
+{
+	m_max_velocity = max_velocity;
+}
+
+void Player::set_rotation(const float& rotation)
+{
+	m_rotation = rotation;
+	m_target_rotation = rotation;
+}
+
 void Player::set_target_rotation(const float& target_rotation)
 {
 	m_target_rotation = target_rotation;
 }
 
-void Player::set_interpolation_values(const float& acceleration_fade, const float& deceleration_fade, const float& rotation_fade)
+void Player::set_interpolation_values(const float& acceleration_fade, const float& deceleration_fade, const float& rotation_fade, const float& input_velocity_multiplier, const float& input_rotation_multiplier)
 {
 	m_acceleration_fade = acceleration_fade;
 	m_deceleration_fade = deceleration_fade;
 	m_rotation_fade = rotation_fade;
+	m_input_velocity_multiplier = input_velocity_multiplier;
+	m_input_rotation_multiplier = input_rotation_multiplier;
 }
+
+void Player::movement_calculation(const float& dt)
+{
+	m_rotation = m_rotation + (m_target_rotation - m_rotation) * fminf((m_rotation_fade * dt), 1);
+
+	float velocity_value = (m_target_velocity - Vector2Length(m_velocity)) > 0
+	? Vector2Length(m_velocity) + (m_target_velocity - Vector2Length(m_velocity)) * fminf((m_acceleration_fade * dt), 1)
+	: Vector2Length(m_velocity) + (m_target_velocity - Vector2Length(m_velocity)) * fminf((m_deceleration_fade * dt), 1);
+
+	m_velocity.x = velocity_value * cos(m_rotation * DEG2RAD);
+	m_velocity.y = velocity_value * sin(m_rotation * DEG2RAD);
+
+	m_position.x += m_velocity.x * dt;
+	m_position.y += m_velocity.y * dt;
+}
+
 
 void Player::update(const float dt)
 {
-	m_rot = m_rot + (m_target_rotation - m_rot) * (m_rotation_fade * dt);
+	if (IsKeyDown(KEY_W)) m_target_velocity += m_target_velocity < m_max_velocity ? m_input_velocity_multiplier  * dt : 0;
+	if (IsKeyDown(KEY_S)) m_target_velocity -= m_target_velocity > 0 ? m_input_velocity_multiplier * dt : 0;
+	if (IsKeyDown(KEY_D) && Vector2Length(m_velocity) > 1) m_target_rotation += m_input_rotation_multiplier * Vector2Length(m_velocity) * dt;
+	if (IsKeyDown(KEY_A) && Vector2Length(m_velocity) > 1) m_target_rotation -= m_input_rotation_multiplier * Vector2Length(m_velocity) * dt;
 
-	float velocity_value = (m_target_velocity - Vector2Length(m_vel)) > 0
-	? Vector2Length(m_vel) + (m_target_velocity - Vector2Length(m_vel)) * (m_acceleration_fade * dt)
-	: Vector2Length(m_vel) + (m_target_velocity - Vector2Length(m_vel)) * (m_deceleration_fade * dt);
-
-	m_vel.x = velocity_value * cos(m_rot * DEG2RAD);
-	m_vel.y = velocity_value * sin(m_rot * DEG2RAD);
-
-	m_pos.x += m_vel.x * dt;
-	m_pos.y += m_vel.y * dt;
+	movement_calculation(dt);
 }
 
-void Player::set_texture_value()
+void Player::set_texture_value(const Texture2D& texture, const float& textures_in_image, const float& rotation_offset, const float& base_scale)
 {
-	// WIP
-
-	m_texture = LoadTexture("ressources/SpaceShip.png");
-	m_textures_in_image = 5;
-	m_rotation_offset = 90;
-	m_source = {0,0, (float)m_texture.width/m_textures_in_image, (float)m_texture.height};
-	m_origin = {(float)m_texture.width/(2*m_textures_in_image), (float)m_texture.height/2};
+	m_texture = texture;
+	m_textures_in_image = textures_in_image;
+	m_rotation_offset = rotation_offset;
+	m_base_scale = base_scale;
 }
 
 
 void Player::render()
 {
-	m_dest = {m_pos.x, m_pos.y, (float)m_texture.width/m_textures_in_image, (float)m_texture.height};
-	DrawTexturePro(m_texture, m_source, m_dest, m_origin, m_rot+m_rotation_offset, WHITE);
+	m_source = {0,0, (float)m_texture.width / m_textures_in_image, (float)m_texture.height};
+	m_dest = {m_position.x, m_position.y, m_source.width * m_base_scale, m_source.height * m_base_scale};
+	m_origin = {m_dest.width / 2, m_dest.height / 2};
+	DrawTexturePro(m_texture, m_source, m_dest, m_origin, m_rotation + m_rotation_offset, WHITE);
 }
--- src/app/player.h
+++ src/app/player.h
@@ -10,37 +10,63 @@ class Player : public PSInterfaces::IRenderable
 public:
 	Player();
 
+	Vector2 position();
+
+	Vector2 velocity();
+
 	float target_velocity();
+
+	float max_velocity();
+
+	float rotation();
 
 	float target_rotation();
 
+	void set_position(const Vector2& position);
+
+	void set_velocity(const Vector2& velocity);
+
 	void set_target_velocity(const float& target_velocity);
+
+	void set_max_velocity(const float& max_velocity);
+
+	void set_rotation(const float& rotation);
 
 	void set_target_rotation(const float& target_rotation);
 
-	void set_interpolation_values(const float& acceleration_fade, const float& deceleration_fade, const float& rotation_fade);
+	void set_interpolation_values(const float& acceleration_fade, const float& deceleration_fade, const float& rotation_fade, const float& input_velocity_multiplier, const float& input_rotation_multiplier);
+
+	void movement_calculation(const float& dt);
 
 	void update(const float dt) override;
 
-	void set_texture_value();
+	void set_texture_value(const Texture2D& texture, const float& textures_in_image, const float& rotation_offset, const float& base_scale);
 
 	void render() override;
 
 private:
-	float m_target_velocity;
-	float m_target_rotation;
-	float m_acceleration_fade;
-	float m_deceleration_fade;
-	float m_rotation_fade;
+	//Base Movement Variables
+	Vector2 m_position = {0};
+	Vector2 m_velocity = {0};
+	float m_max_velocity = 0;
+	float m_rotation = 0;
 
-	Texture2D m_texture;
-	float m_textures_in_image;
-	float m_rotation_offset;
-	Rectangle m_source;
-	Rectangle m_dest;
-	Vector2 m_origin;
-	
-	float m_rot;
-	Vector2 m_vel;
-	Vector2 m_pos;
+	//Interpolation Values for the Movement Calculation
+	float m_target_velocity = 0;
+	float m_target_rotation = 0;
+	float m_acceleration_fade = 0;
+	float m_deceleration_fade = 0;
+	float m_rotation_fade = 0;
+	float m_input_velocity_multiplier = 0;
+	float m_input_rotation_multiplier = 0;
+
+	//Variables for Texture Rendering
+	Texture2D m_texture = {0};
+	float m_textures_in_image = 1; //For one dimensional Texture with Animation
+	float m_rotation_offset = 0;
+	float m_base_scale = 1;
+	Rectangle m_source = {0};
+	Rectangle m_dest = {0};
+	Vector2 m_origin = {0};
+
 };
