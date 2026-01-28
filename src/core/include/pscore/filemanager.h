140d23eb2949f29896c72f9b4f9a558e1f83b04d
Boss-Block <marius.wolfgramm@icloud.com>
2026-01-28 10:21:36 +0100

    MAGO-88 added filewriter (#9)
    * File Writer
    
    * File Writer 2
    
    * File Writer 3
    
    ---------
    
    Co-authored-by: Noah F <114858797+no8f@users.noreply.github.com>

---
+++ src/core/include/pscore/filemanager.h
@@ -0,0 +1,21 @@
+//
+// Created by mariu on 09.01.2026.
+//
+
+#pragma once
+#include <string>
+
+class Filemanager
+{
+
+public:
+	void ensurefileexists(std::string filename);
+
+	filemanager(std::string filename);
+
+	void write(const std::string& mes);
+	std::string load();
+
+private:
+	std::string filename;
+};
