c43c93b39371fc2d72d4866ad366f9867b346953
no8f <no8f@users.noreply.github.com>
2026-02-03 12:06:49 +0000

    deploy: b50ff6689bdcbcd3625ea5f3f546381a3f98a618

--- classPSCore_1_1sprites_1_1SpriteSheetAnimation.js
+++ classPSCore_1_1sprites_1_1SpriteSheetAnimation.js
@@ -6,5 +6,6 @@ var classPSCore_1_1sprites_1_1SpriteSheetAnimation =
     [ "get_sprite_sheet_animation_index", "classPSCore_1_1sprites_1_1SpriteSheetAnimation.html#ae5c69f2be23d20507de0d524831da1da", null ],
     [ "get_sprite_sheet_frame_index", "classPSCore_1_1sprites_1_1SpriteSheetAnimation.html#ae3878238fe7f8b3f771343c6c8af2ada", null ],
     [ "set_animation_at_index", "classPSCore_1_1sprites_1_1SpriteSheetAnimation.html#a835d2b72ce3704c1e286af4051da15c7", null ],
+    [ "set_new_animation_framerate", "classPSCore_1_1sprites_1_1SpriteSheetAnimation.html#af3cdd35a926a61788e1732697836a553", null ],
     [ "update_animation", "classPSCore_1_1sprites_1_1SpriteSheetAnimation.html#a016b5d0aa8a6cccb3db026275076088b", null ]
 ];
--- search/functions_9.js
+++ search/functions_9.js
@@ -1,11 +1,12 @@
 var searchData=
 [
   ['set_5fanimation_5fat_5findex_0',['set_animation_at_index',['../classPSCore_1_1sprites_1_1SpriteSheetAnimation.html#a835d2b72ce3704c1e286af4051da15c7',1,'PSCore::sprites::SpriteSheetAnimation']]],
-  ['set_5fplayer_5fposition_5fto_5fopposite_5fborder_1',['set_player_position_to_opposite_border',['../namespacemisc_1_1map.html#a9be0704e6416db02a479070dc8cec015',1,'misc::map']]],
-  ['set_5ftarget_5ffps_2',['set_target_fps',['../classPSCore_1_1DeltaTimeManager.html#a255126b97e19205841867e689918af6f',1,'PSCore::DeltaTimeManager']]],
-  ['set_5fwrap_5faround_5fmode_3',['set_wrap_around_mode',['../namespacemisc_1_1map.html#a6da7cb8cb5c63049d7007e8c72fce104',1,'misc::map']]],
-  ['spawn_5fnew_5fplayer_5fat_5fopposite_5fborder_4',['spawn_new_player_at_opposite_border',['../namespacemisc_1_1map.html#a73d666d5d54fa4dbf0e5969f2ce94e7f',1,'misc::map']]],
-  ['spritesheetanimation_5',['SpriteSheetAnimation',['../classPSCore_1_1sprites_1_1SpriteSheetAnimation.html#a1bf8dba87e45334918a2d2158e611672',1,'PSCore::sprites::SpriteSheetAnimation']]],
-  ['stop_6',['stop',['../classPSCore_1_1Application.html#a2e5dee1fbe413783d3500fe8da567ad4',1,'PSCore::Application']]],
-  ['submit_5frenderable_7',['submit_renderable',['../classPSCore_1_1Renderer.html#a3bfa539d23da7eb4afbe0bc142938744',1,'PSCore::Renderer']]]
+  ['set_5fnew_5fanimation_5fframerate_1',['set_new_animation_framerate',['../classPSCore_1_1sprites_1_1SpriteSheetAnimation.html#af3cdd35a926a61788e1732697836a553',1,'PSCore::sprites::SpriteSheetAnimation']]],
+  ['set_5fplayer_5fposition_5fto_5fopposite_5fborder_2',['set_player_position_to_opposite_border',['../namespacemisc_1_1map.html#a9be0704e6416db02a479070dc8cec015',1,'misc::map']]],
+  ['set_5ftarget_5ffps_3',['set_target_fps',['../classPSCore_1_1DeltaTimeManager.html#a255126b97e19205841867e689918af6f',1,'PSCore::DeltaTimeManager']]],
+  ['set_5fwrap_5faround_5fmode_4',['set_wrap_around_mode',['../namespacemisc_1_1map.html#a6da7cb8cb5c63049d7007e8c72fce104',1,'misc::map']]],
+  ['spawn_5fnew_5fplayer_5fat_5fopposite_5fborder_5',['spawn_new_player_at_opposite_border',['../namespacemisc_1_1map.html#a73d666d5d54fa4dbf0e5969f2ce94e7f',1,'misc::map']]],
+  ['spritesheetanimation_6',['SpriteSheetAnimation',['../classPSCore_1_1sprites_1_1SpriteSheetAnimation.html#a1bf8dba87e45334918a2d2158e611672',1,'PSCore::sprites::SpriteSheetAnimation']]],
+  ['stop_7',['stop',['../classPSCore_1_1Application.html#a2e5dee1fbe413783d3500fe8da567ad4',1,'PSCore::Application']]],
+  ['submit_5frenderable_8',['submit_renderable',['../classPSCore_1_1Renderer.html#a3bfa539d23da7eb4afbe0bc142938744',1,'PSCore::Renderer']]]
 ];
