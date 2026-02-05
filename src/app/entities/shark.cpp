29c9ca4b794a40d900a8207553bef4c0d61b03b7
Noah F <114858797+no8f@users.noreply.github.com>
2026-02-05 12:12:42 +0100

    MAGO-90 added water, fixed shark not rendering correctly

--- src/app/entities/shark.cpp
+++ src/app/entities/shark.cpp
@@ -36,7 +36,7 @@ void Fin::render()
 		auto tex = m_shark->m_shark_sprite;
 		vp->draw_in_viewport(
 				tex->m_s_texture, m_shark->m_animation_controller.get_source_rectangle(1).value_or(Rectangle{0}), m_shark->m_pos,
-				m_shark->m_shark_rotation + 90, RED
+				m_shark->m_shark_rotation + 90, WHITE
 		);
 	}
 }
@@ -102,8 +102,8 @@ Shark::Shark() : PSInterfaces::IEntity("shark")
 	m_body = std::make_shared<Body>(this);
 	m_fin  = std::make_shared<Fin>(this);
 
-	m_body->propose_z_index(-1);
-	m_fin->propose_z_index(1);
+	m_body->propose_z_index(-20);
+	m_fin->propose_z_index(5);
 
 	// Has an droppable upgrade
 	m_marked = PSUtils::gen_rand(1, 100) > 50;
@@ -133,10 +133,10 @@ void Shark::init(std::shared_ptr<Shark> self, const Vector2& pos)
 
 Shark::~Shark()
 {
-	// if ( m_marked ) {
-	// 	PS_LOG(LOG_INFO, "Dropped an upgrade");
-	// 	// TODO: implement loot drop
-	// }
+	if ( m_marked ) {
+		PS_LOG(LOG_INFO, "Dropped an upgrade");
+		// TODO: implement loot drop
+	}
 }
 
 void Shark::update(float dt)
@@ -263,8 +263,6 @@ void Shark::draw_debug()
 
 void Shark::render()
 {
-	m_body->render();
-	m_fin->render();
 }
 
 void Shark::set_pos(const Vector2& pos)
@@ -292,3 +290,10 @@ std::optional<Vector2> Shark::position() const
 {
 	return m_pos;
 };
+
+void Shark::set_is_active(bool active)
+{
+	is_active_ = active;
+	m_body->set_is_active(active);
+	m_fin->set_is_active(active);
+}
