3820a76b5e536f9412eaa04282a7922da60f5e7f
Noel Knauf <noel.knauf@gmx.de>
2026-02-09 17:49:22 +0100

    stuff

--- src/app/entities/director.cpp
+++ src/app/entities/director.cpp
@@ -405,8 +405,8 @@ void FortunaDirector::Bounty::subtract_bounty(const int amount)
 void FortunaDirector::increase_difficulty(int bounty)
 {
 	// Increase shark spawn rate and limit based on bounty
-	_p->shark_spawner->set_interval(std::max(0.001f, _p->shark_spawner->interval() - 0.05f * (bounty / 100)));
-	_p->shark_spawner->set_limit(std::min(999, _p->shark_spawner->limit() + (bounty / 100)));
+	_p->shark_spawner->set_interval(std::max(0.1f, _p->shark_spawn_time - 0.05f * (bounty / 100)));
+	_p->shark_spawner->set_limit(std::min(100, _p->shark_limit + (bounty / 100)));
 	printf("Bounty: %d, Shark Spawn Interval: %.2f, Shark Limit: %d\n", bounty, _p->shark_spawner->interval(), _p->shark_spawner->limit());
 }
 
