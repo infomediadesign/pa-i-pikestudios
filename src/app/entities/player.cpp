efa14798658c500295bc70ba74f99816435a0fcd
Spielemeistr <mppb1908@gmx.de>
2026-02-18 13:07:28 +0100

    Add emissive Shader for the Sails

--- src/app/entities/player.cpp
+++ src/app/entities/player.cpp
@@ -35,11 +35,11 @@ Player::Player() : PSInterfaces::IEntity("player")
 	m_sprite = PRELOAD_TEXTURE(ident_, "resources/entity/player.png", frame_grid);
 
 	m_animation_controller = PSCore::sprites::SpriteSheetAnimation(
-			FETCH_SPRITE_TEXTURE(ident_), {{1, 1, PSCore::sprites::KeyFrame, 1},
+			FETCH_SPRITE_TEXTURE(ident_), {{2, 1, PSCore::sprites::KeyFrame, 1},
 										   {3, 1, PSCore::sprites::KeyFrame, 3},
 										   {3, 1, PSCore::sprites::KeyFrame, 3},
 										   {3, 1, PSCore::sprites::KeyFrame, 3},
-										   {10, 0.1, PSCore::sprites::Forward, 1}}
+										   {10, 0.1, PSCore::sprites::Forward, 3}}
 	);
 
 	m_animation_controller.add_animation_at_index(0, 1);
@@ -139,8 +139,9 @@ void Player::update(const float dt)
 			m_target_velocity = 0;
 			m_velocity		  = {0, 0};
 
-			if ( m_animation_controller.get_sprite_sheet_frame_index(1) == 9 ) {
+			if ( m_animation_controller.get_sprite_sheet_frame_index(3) == 9 ) {
 				set_is_active(false);
+				m_sails->set_is_active(false);
 			}
 		}
 	}
@@ -212,11 +213,12 @@ void Player::on_death()
 		score_layer->player_name_input = gApp()->current_player_name();
 	}
 
-	m_sails->set_is_active(false);
 	for ( const auto& cannon: m_cannon_container ) {
 		cannon->set_is_active(false);
 	}
-	m_animation_controller.set_animation_at_index(4, 0, 1);
+
+	m_animation_controller.set_animation_at_index(0, 1, 1);
+	m_animation_controller.set_animation_at_index(4, 0, 3);
 
 	gApp()->get_layer<AppLayer>()->set_can_open_pause_menu(false);
 	gApp()->push_layer<DeathScreenLayer>();
@@ -639,10 +641,16 @@ void Player::set_fire_mode(FireMode mode)
 //
 Sails::Sails(Player* player) : PSInterfaces::IEntity("player_sails"), m_player(player)
 {
+	Vector2 frame_grid{3, 4};
+	m_sprite = PRELOAD_TEXTURE(ident_, "resources/emissive/player_emissive.png", frame_grid);
+
+	m_emissive_texture_location = GetShaderLocation(m_emissive_shader, "texture_emissive");
+	SetShaderValue(m_emissive_shader, GetShaderLocation(m_emissive_shader, "emissive_color"), &m_emissive_color, SHADER_UNIFORM_VEC3);
 }
 
 Sails::~Sails()
 {
+	UnloadShader(m_emissive_shader);
 }
 
 void Sails::update(float dt)
@@ -651,6 +659,10 @@ void Sails::update(float dt)
 
 void Sails::render()
 {
+	BeginShaderMode(m_emissive_shader);
+
+	SetShaderValueTexture(m_emissive_shader, m_emissive_texture_location, m_sprite->m_s_texture);
+
 	if ( auto& vp = gApp()->viewport() ) {
 		auto texture = m_player->m_sprite;
 		vp->draw_in_viewport(
@@ -658,6 +670,8 @@ void Sails::render()
 				m_player->m_rotation + m_player->m_rotation_offset, WHITE
 		);
 	}
+
+	EndShaderMode();
 }
 
 void Sails::draw_debug()
