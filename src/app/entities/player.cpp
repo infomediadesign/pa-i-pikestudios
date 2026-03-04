9d3e4ab17331cfe9893208ea7b4145b128edbe17
Spielemeistr <mppb1908@gmx.de>
2026-03-04 10:59:47 +0100

    Cleaning up

--- src/app/entities/player.cpp
+++ src/app/entities/player.cpp
@@ -735,8 +735,6 @@ Sails::Sails(Player* player) : PSInterfaces::IEntity("player_sails"), m_player(p
 		auto& settings				= PSCore::SettingsManager::inst()->settings["user_preferences"];
 		m_emissive_color = PSUtils::color_to_vector3(std::get<int>(settings->value("player_color").value_or(0xFFFFFF)));
 	}
-
-	//m_emissive_color = {255,0,255};
 	
 	m_emissive_texture_location = GetShaderLocation(m_emissive_shader, "texture_emissive");
 	SetShaderValue(m_emissive_shader, GetShaderLocation(m_emissive_shader, "emissive_color"), &m_emissive_color, SHADER_UNIFORM_VEC3);
